be97f7d7a3af5a52880bc5ea2fa2baf765f732e9
user <rfrjcbr2@gmail.com>
2018-09-27 17:59:59 +0900

    protocol

---
+++ app/src/main/java/com/example/user/secondfootballapp/user/activity/ProtocolEdit.java
@@ -0,0 +1,13 @@
+package com.example.user.secondfootballapp.user.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProtocolEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.protocol_edit);
+    }
+}

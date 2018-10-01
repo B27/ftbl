392636b58c0aa57cca708c63d2774a03a6ae9bf8
user <rfrjcbr2@gmail.com>
2018-10-01 19:02:27 +0800

    protocol

---
+++ app/src/main/java/com/example/user/secondfootballapp/players/activity/PlayerInv.java
@@ -0,0 +1,13 @@
+package com.example.user.secondfootballapp.players.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerInv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_inv);
+    }
+}
---
+++ app/src/main/java/com/example/user/secondfootballapp/user/activity/ConfirmProtocol.java
@@ -0,0 +1,15 @@
+package com.example.user.secondfootballapp.user.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.secondfootballapp.R;
+
+public class ShowAndConfirmProtocol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_and_confirm_protocol);
+    }
+}

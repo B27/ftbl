3b4ae597ea7906bdec72e187f3e7cad6ba019937
user <rfrjcbr2@gmail.com>
2018-10-05 17:52:33 +0800

    post image

---
+++ app/src/main/java/com/example/user/secondfootballapp/tournament/activity/CommandAbbrev.java
@@ -0,0 +1,4 @@
+package com.example.user.secondfootballapp.tournament.activity;
+
+public class CommandAbbrev {
+}
---
+++ app/src/main/java/com/example/user/secondfootballapp/user/activity/EditEvent.java
@@ -0,0 +1,13 @@
+package com.example.user.secondfootballapp.user.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_event);
+    }
+}

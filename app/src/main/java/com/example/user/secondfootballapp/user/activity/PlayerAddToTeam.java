c5bfd9e2f5fdc4f4711b5b4a57239f6a8c460c3f
user <rfrjcbr2@gmail.com>
2018-10-24 19:10:57 +0800

    fix ui

---
+++ app/src/main/java/com/example/user/secondfootballapp/user/activity/PlayerAddToTeam.java
@@ -0,0 +1,13 @@
+package com.example.user.secondfootballapp.user.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerAddToTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_add_to_team);
+    }
+}
